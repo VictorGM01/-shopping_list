139ac3aaae8e44006417f6e2f410978755d57a23
VictorGM01 <victormarques8801@gmail.com>
2024-03-12 18:07:10 -0300

    feat: cria página de login

--- src/frontend/src/pages/Login/index.jsx
+++ src/frontend/src/pages/Login/index.jsx
@@ -2,12 +2,39 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import styles from "./login.module.css";
 import Box from "@mui/material/Box";
+import { useContext } from "react";
+import { useEffect } from "react";
+
+import { Context } from "../../contexts/Auth";
 
 export default function Login() {
+  const { handleLogin, handleLogout } = useContext(Context);
+
+  useEffect(() => {
+    handleLogout();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onFinish = async (values) => {
+    handleLogin(values);
+  };
+
   return (
     <main className={styles.container}>
       <h1>Login</h1>
-      <Box component="form" noValidate autoComplete="off" className={styles.form}>
+      <Box
+        component="form"
+        noValidate
+        autoComplete="off"
+        className={styles.form}
+        onSubmit={(e) => {
+          e.preventDefault();
+          onFinish({
+            email: e.target[0].value,
+            senha: e.target[1].value,
+          });
+        }}
+      >
         <TextField label="Usuário" size="small" variant="standard" />
         <TextField
           label="Senha"
@@ -15,7 +42,7 @@ export default function Login() {
           size="small"
           variant="standard"
         />
-        <Button variant="contained" color="primary" size="large">
+        <Button variant="contained" color="primary" size="large" type="submit">
           Entrar
         </Button>
       </Box>
--- src/frontend/src/routes.jsx
+++ src/frontend/src/routes.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import { AuthProvider } from "./contexts/Auth";
 
 export default function AppRoute() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
 
-        <Route path="*" element={<h1>Página não encontrada</h1>} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="*" element={<h1>Página não encontrada</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
